15acc3a2f6f2dd6a6a432dab84cefacc40c8498e
Farazi Ahmed <77005336+Farazi-Ahmed@users.noreply.github.com>
2021-01-06 07:49:32 -0500

    Add files via upload

---
+++ problem-solved-11.py
@@ -0,0 +1,12 @@
+# Question 11 : Assume there are two variables a and b. Take Values of these variables from the user. Swap the values!
+
+x=int(input("Value of x: "))
+y=int(input("Value of y: "))
+print(x)
+print(y)
+
+t1 = x
+x = y
+y = t1
+print(x)
+print(y)
