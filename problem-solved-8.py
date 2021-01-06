14ce48225ae7fb08756553c7963d92de217a4e6a
Farazi Ahmed <77005336+Farazi-Ahmed@users.noreply.github.com>
2021-01-06 07:46:10 -0500

    Add files via upload

---
+++ problem-solved-8.py
@@ -0,0 +1,10 @@
+# Question 8 : A leading newspaper pays all their freelance writers at a rate of $500 per published article. 
+# Draw the flowchart for a program that will read the number of published articles for one writer, and print the total fees for that writer.
+
+article = int(input("Provide number of articles: "))
+
+Rate = 500
+
+TotalFees = article * Rate
+
+print(TotalFees)
