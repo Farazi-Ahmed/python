a917642a1357e9751745c9badd27c57260e8dc42
Farazi Ahmed <77005336+Farazi-Ahmed@users.noreply.github.com>
2021-01-06 07:38:48 -0500

    Add files via upload

---
+++ problem-solved-3.py
@@ -0,0 +1,7 @@
+# Question 3 : Write a flowchart for printing area of a rectangle taking height and width input from the user.
+
+h = int(input("Provide Height: "))
+w = int(input("Provide Width: "))
+
+area = h*w
+print("Area is "+str(area))
