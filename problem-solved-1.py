47d15ce1bd2f659bf8538864783a68679d9a93e1
Farazi Ahmed <77005336+Farazi-Ahmed@users.noreply.github.com>
2021-01-06 07:31:18 -0500

    Update problem-solved-1.py

--- problem-solved-1.py
+++ problem-solved-1.py
@@ -1,4 +1,5 @@
-# Question 1
+# Question 1: Draw the flowchart of a program that reads a student’s mark for a single subject, and prints out “Pass” if the student got more than 50, and “You shall not pass” otherwise.
+
 a=int(input("Provide your mark: "))
 if a >= 50:
   print("Pass")
