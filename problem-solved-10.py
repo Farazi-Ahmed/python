2cad22e06a0593ba6950f2dc92de1009d479f89e
Farazi Ahmed <77005336+Farazi-Ahmed@users.noreply.github.com>
2021-01-06 07:48:24 -0500

    Add files via upload

---
+++ problem-solved-10.py
@@ -0,0 +1,10 @@
+# Question 10 : Draw the flowchart for the following:
+# 1) Ask the user to enter the name of his favorite car.
+# 2) Display the name of the user’s favorite car 4 times.
+
+car = input("Favorite car: ")
+
+print(car)
+print(car)
+print(car)
+print(car)
