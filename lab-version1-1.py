0ec0582e3c5cf9e79f17e04236baa69cee00c8f8
Farazi Ahmed <77005336+Farazi-Ahmed@users.noreply.github.com>
2021-01-09 08:50:20 -0500

    Add files via upload

---
+++ lab-version1-1.py
@@ -0,0 +1,10 @@
+# Question 2 Draw the flowchart of a program that reads two numbers from the user, and prints their sum, product and result of subtracting 2nd number from 1st number.
+
+a = int(input("First Number: "))
+b = int(input("Second Number: "))
+
+sum = a+b
+product = a*b
+subtract = a-b
+
+print(sum, product, subtract)
